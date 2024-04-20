2b0c452e500c17d9f69e1a095e32fd1d090f8db8
Garrett <dalygarrett514@yext.com>
2024-04-19 22:50:57 -0400

    nocors

--- src/FileDrop.js
+++ src/FileDrop.js
@@ -59,10 +59,12 @@ const FileDrop = () => {
         try {
           const response = await fetch(`https://thingproxy.freeboard.io/fetch/https://api.yext.com/v2/accounts/me/scan?api_key=${apiKey}&v=20240412`, {
             method: 'POST',
+
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify(requestData)
+            body: JSON.stringify(requestData),
+            mode: 'no-cors' // Add this line to set the request mode to 'no-cors'
           });
   
           if (!response.ok) {
